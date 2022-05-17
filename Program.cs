b42709be9cccccf94416f9f9075af0fa236aa439
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-05-17 15:00:40 +0200

    add print all list

--- Program.cs
+++ Program.cs
@@ -8,11 +8,22 @@ namespace csharp_agenzia // Note: actual namespace depends on the project name.
         {
             Box immobile1 = new Box(4 ,"123512", "santa margherita", "27011","belgioiso", 50);
             
-            Console.WriteLine(immobile1.ToString());
+           // Console.WriteLine(immobile1.ToString());
 
             Agenzia az = new Agenzia("lorenzo");
-
             az.AddImmobile(immobile1);
+
+            //ricerca ville
+            Console.WriteLine("inserisci il codice dell'immobile per la ricerca:");
+            string sSearch = Console.ReadLine();
+
+            List<Immobile> immobileList = az.SearchImmobili(sSearch);
+            foreach(Immobile i in immobileList)
+            {
+                Console.WriteLine(i.ToString());
+                Console.WriteLine("----------------------------------------------------");
+            }
+            Console.WriteLine("ho finito");
             
         }
     }
